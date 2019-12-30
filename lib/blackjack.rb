10ddeb894aaefe350c33747978ffd9fdc9b1a129
Aaron Burmeister <f34r91@gmail.com>
2019-12-30 19:00:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,34 +7,44 @@ def deal_card
   rand(1..11)
 end
 
-puts deal_card
-
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card1 = deal_card
+  card2 = deal_card
+  display_card_total(card1 + card2)
+  return card1 + card2
 end
 
-def hit?
-  # code hit? here
+def hit? (card_total)
+  prompt_user
+  answer = get_user_input
+  if answer == "s"
+    return card_total
+  elsif answer == "h"
+    card_total += deal_card
+    return card_total
+  else
+    invalid_command("Please enter a valid command")
+    prompt_user
 end
 
-def invalid_command
-  # code invalid_command here
+def invalid_command(error_message)
+  puts error_message
 end
 
 #####################################################
